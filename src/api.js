7361b2ede549933fee64031f5447bee65a14f133
Angel Aguirre <angelaguirrel@hotmail.com>
2022-02-10 23:07:29 -0600

    Corrigiendo error de formato por el linter

--- src/api.js
+++ src/api.js
@@ -30,13 +30,15 @@ function getAssetHistory(coin) {
     now.setDate(now.getDate() - 1);
     const start = now.getTime();
     return fetch(
-        `${url}/assets/${coin}/history?interval=h1&start=${start}&end=${end}`, {
-        mode: 'cors',
-        method: 'GET',
-        headers: {
-            Accept: 'application/json',
-        },
-    })
+        `${url}/assets/${coin}/history?interval=h1&start=${start}&end=${end}`,
+        {
+            mode: 'cors',
+            method: 'GET',
+            headers: {
+                Accept: 'application/json',
+            },
+        }
+    )
         .then((res) => res.json())
         .then((res) => res.data);
 }
